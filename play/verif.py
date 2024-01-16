af140b1fe94fabc394753862937cdf84483edee0
Ilies HARKOU <iharkou@EDUHDF.FR>
2024-01-16 10:15:22 +0100

    fixed win

--- play/verif.py
+++ play/verif.py
@@ -8,11 +8,12 @@ def horizontal(grille:Grille) -> Tuple[bool,int]:
         compare:int = 0
         nb_compare:int = 0
         for elt in lst:
-            if compare == elt:
-                nb_compare += 1
-            else:
-                compare = elt
-                nb_compare = 0
-            if nb_compare == 4:
-                return True,elt
+            if compare != 0:
+                if compare == elt:
+                    nb_compare += 1
+                else:
+                    compare = elt
+                    nb_compare = 0
+                if nb_compare == 4:
+                    return True,elt
         return False,0
