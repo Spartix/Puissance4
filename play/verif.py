ce7d93e778c84f9bfdd802bfdb916b7db6b8c578
Ilies HARKOU <iharkou@EDUHDF.FR>
2024-01-22 09:56:12 +0100

    fix

--- play/verif.py
+++ play/verif.py
@@ -43,16 +43,28 @@ def diagonale_droit(grille:Grille,start):
         else:
             compare = grille[start][start]
             nb_compare = 1
-        if nb_compare == 4 and compare !=0:
+        if nb_compare == 4 and compare != 0 :
             return True,compare
+        start += 1
     return False,0
-        
+
+def diagonale_droit_second(grille:Grille,start:int):
+    compare = 0
+    nb_compare = 0
+    while start < 6:
+        if compare == grille[start][start]:
+            nb_compare += 1
+        else:
+            compare = grille[start][start]
+            nb_compare = 1
+        if nb_compare == 4 and compare != 0 :
+            return True,compare
+        start += 1
+    return False,0
         
 
 def diagonale(grille:Grille):
-    isTrue = (False,0)
     for i in range(3):
-        if isTrue[0] or diagonale_droit(grille,i)[0]:
-            isTrue = diagonale_droit(grille,i)
-    return isTrue
-    
+        if diagonale_droit(grille,i)[0]:
+            return diagonale_droit(grille,i)
+    return False
