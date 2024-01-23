fc1e8ee58635bcf70f81962f1439ff5f0517fd4c
Ilies HARKOU <iharkou@EDUHDF.FR>
2024-01-23 11:46:10 +0100

    diago

--- play/verif.py
+++ play/verif.py
@@ -35,19 +35,17 @@ def vertical(grille:Grille):
     return False,0
 
 
-def diagonale_droit(grille:Grille,start:int):
-    compare = 0
+def diagonale_droit(grille:Grille,start_ligne:int,start_colone):
+    comaprer = 0
     nb_compare = 0
-    while start < 5:
-            if compare == grille[start][start+1]:
-                nb_compare += 1
-            else:
-                compare = grille[start][start+1]
-                nb_compare = 1
-            if nb_compare == 4 and compare != 0 :
-                return True,compare
-            start += 1
-    return False,0
+    for _ in range(6):
+        if(grille[start_ligne][start_colone] == comaprer):
+            nb_compare += 1
+        else:
+            comaprer = grille[start_ligne][start_colone]
+        start_ligne -= 1
+        start_colone +=1
+        
     
 def diagonale_gauche(grille:Grille,start:int):
     compare = 0
@@ -68,7 +66,7 @@ def diagonale(grille:Grille):
     for i in range(3):
         if diagonale_droit(grille,i)[0]:
             return diagonale_droit(grille,i)
-    for a in range(5,-1,-1):
-        if diagonale_gauche(grille,a)[0]:
-            return diagonale_gauche(grille,a)
+    for caca in range(5):
+        for proute in range(5):
+            diagonale_droit(caca,proute)
     return False,0
